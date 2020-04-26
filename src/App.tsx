a11a34291b263cc13c6020a423fc8dd2d583e757
Giacomo Petronio <4199330+hijackit@users.noreply.github.com>
2020-04-26 18:00:20 +0200

    better defaults

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import "./index.css";
 export function App() {
   return (
     <div className="main">
-        <h1>Tremplate</h1>
-        <p>Welcome to the tremplate</p>
+        <h1>changeme</h1>
     </div>
   )
 }
